16bfc451213d8386e6d687f5672120742dc99a92
hajidlukito <noordinteroris@rocketmail.com>
2018-12-12 15:34:59 +0700

    this is my first comit

---
+++ UrlChecker.java
@@ -0,0 +1,21 @@
+package
+import java
+import java
+import java
+
+class urlcjhecker
+	public void
+pattern my[pattern
+		scanner
+		sout
+		string
+		matcher
+		boolean
+
+	if
+		sout
+			else
+		sout
+}
+}
+}
